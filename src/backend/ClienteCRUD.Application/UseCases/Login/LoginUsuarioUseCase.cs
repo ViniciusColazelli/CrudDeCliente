b0b9053f8efd4507cca2ac883f70c2f4f0e08bc8
Vinicius colazelli <vinicolazelli@yahoo.com.br>
2026-04-26 11:02:49 -0300

    Fix: consertando feature pra pegar profile do cliente com cookies.

--- src/backend/ClienteCRUD.Application/UseCases/Login/LoginUsuarioUseCase.cs
+++ src/backend/ClienteCRUD.Application/UseCases/Login/LoginUsuarioUseCase.cs
@@ -3,6 +3,7 @@ using ClienteCRUD.Communication.Responses;
 using ClienteCRUD.Domain.Repositories;
 using ClienteCRUD.Domain.Security.Criptography;
 using ClienteCRUD.Exceptions.ExceptionBase;
+using Microsoft.AspNetCore.Http;
 
 namespace ClienteCRUD.Application.UseCases.Login
 {
@@ -10,19 +11,22 @@ namespace ClienteCRUD.Application.UseCases.Login
     {
         private readonly IUserRepository _userRepository;
         private readonly ISenhaCriptografada _senhaCriptografada;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public LoginUsuarioUseCase(IUserRepository userRepository,ISenhaCriptografada senhaCriptografada)
+        public LoginUsuarioUseCase(IUserRepository userRepository,ISenhaCriptografada senhaCriptografada, IHttpContextAccessor httpContextAccessor)
         {
             _userRepository = userRepository;
             _senhaCriptografada = senhaCriptografada;
+            _httpContextAccessor = httpContextAccessor;
         }
         public async Task<ResponseUsuarioRegistrado> Execute(RequestLoginUsuario request)
         {
             var senhaCriptografada = _senhaCriptografada.Criptografia(request.Senha);
 
             var user = await _userRepository.GetEmailAndPassword(request.Email, senhaCriptografada) ?? throw new ErroEmLoginException();// esses dois interrogativos são para verificar
-                                                                                                                                   // caso seja nulo o metodo ou algum parametro do GetEmailAndPassword(),
-                                                                                                                                // se for nulo, lança a exceção ErroEmLoginException se não, segue o fluxo
+                                                                                                                                        // caso seja nulo o metodo ou algum parametro do GetEmailAndPassword(),
+            _httpContextAccessor.HttpContext!.Session.SetInt32("ClienteId", user.Id);                                                   // se for nulo, lança a exceção ErroEmLoginException se não, segue o fluxo
+
             return new ResponseUsuarioRegistrado()
             {
                 Nome = user.Nome
